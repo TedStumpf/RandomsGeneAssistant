e658448c42b2a7204bd30d1c955ef247d39d7c09
Theodore Stumpf <theodorestumpf@gmail.com>
2022-11-01 20:34:41 -0400

    Added genebank fill display
    Added a line to the Gene Assembler that displays the fill level of connected gene banks.

--- source/GeneStatusColor.cs
+++ source/GeneStatusColor.cs
@@ -182,4 +182,34 @@ namespace RandomsGeneAssistant
             }
         }
     }
+
+    [HarmonyPatch]
+    public static class GeneAssemblerFilledSlots
+    {
+        //  Taken from the Genebank
+        [HarmonyPatch(typeof(Building_GeneAssembler), nameof(Building_GeneAssembler.GetInspectString))]
+        [HarmonyPostfix]
+        public static void GetInspectString_Postfix(Building_GeneAssembler __instance, ref string __result)
+        {
+            int filled = 0;
+            int total = 0;
+
+            //  
+            List<Thing> connectedFacilities = __instance.ConnectedFacilities;
+            if (connectedFacilities != null)
+            {
+                foreach (Thing thing in connectedFacilities)
+                {
+                    CompGenepackContainer comp = thing.TryGetComp<CompGenepackContainer>();
+                    if (comp != null)
+                    {
+                        filled += comp.ContainedGenepacks.Count;
+                        total += comp.Props.maxCapacity;
+                    }
+                }
+            }
+
+            __result += "\nFilled Slots: " + filled + "/" + total;
+        }
+    }
 }
